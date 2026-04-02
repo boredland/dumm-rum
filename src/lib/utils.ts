241b0d346522c3e4802930ae3cc6948bc98fdb29
Jonas Strasel <info@jonas-strassel.de>
2026-04-02 18:12:03 +0200

    Replace Frankfurt (Main) with FFM in all station name displays

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -85,7 +85,7 @@ export function trendColor(arrow: string): string {
 }
 
 export function shortStationName(name: string): string {
-	return name.replace(/^Frankfurt \(Main\)\s*/i, "");
+	return name.replace(/^Frankfurt \(Main\)\s*/i, "FFM ");
 }
 
 export const PLANNED_FREQUENCY_MIN = 15;
